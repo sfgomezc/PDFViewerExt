a0e4ca857f82c3cabaa22f2cd32714426734ddf2
Steven Gomez <steven.gomez.c@gmail.com>
2023-07-26 13:39:39 -0500

    Agregar archivos de proyecto.

---
+++ PDFViewerExt/PDFViewerExt/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PDFViewerExt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LauncherButtonOnClicked(object sender, EventArgs e)
+        {
+            var filePath = await DownloadPdfFileAsync();
+
+            if (filePath != null)
+            {
+                await Launcher.OpenAsync(new OpenFileRequest
+                {
+                    File = new ReadOnlyFile(filePath)
+                });
+            }
+        }
+
+        private async void OpenButtonOnClicked(object sender, EventArgs e)
+        {
+            var filePath = await GeneratePDFFile();
+
+            if (File.Exists(filePath))
+            {
+                await Launcher.OpenAsync(new OpenFileRequest
+                {
+                    File = new ReadOnlyFile(filePath)
+                });
+            }
+        }
+
+        private async Task<string> DownloadPdfFileAsync()
+        {
+            var filePath = Path.Combine(FileSystem.AppDataDirectory, "test.pdf");
+
+            if (File.Exists(filePath))
+                return filePath;
+
+            var httpClient = new HttpClient();
+            var pdfBytes = await httpClient.GetByteArrayAsync("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf");
+
+            try
+            {
+                File.WriteAllBytes(filePath, pdfBytes);
+
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+            }
+
+            return null;
+        }
+
+        private async Task<string> GeneratePDFFile()
+        {
+            var filePath = Path.Combine(FileSystem.AppDataDirectory, "PDFSample.pdf");
+
+            if (File.Exists(filePath))
+                return filePath;
+
+            Assembly assembly = GetType().GetTypeInfo().Assembly;
+
+            using (Stream stream = assembly.GetManifestResourceStream("PDFViewerExt.Content.PDFSample.pdf"))
+            {
+                if (stream != null)
+                {
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        try
+                        {
+
+                            stream.CopyTo(memoryStream);
+                            var pdfBytes = memoryStream.ToArray();
+                            File.WriteAllBytes(filePath, pdfBytes);
+
+                            return filePath;
+                        }
+                        catch (Exception ex)
+                        {
+                            await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# PDFViewerExt
+Visor de archivos PDF que utiliza Xamarin.Essentials para abrir externamente con aplicaciones del dispositivo:
+
+- Permite descargar un archivo PDF desde URL e inmediatamente lo abre con app externa que se tenga en el dispositivo.
+- Permite abrer un archivo PDF local (embebido en el proyecto) y posteriormente se abre con app externa que se tenga en el dispositivo.
+
+
+<img src="https://github.com/sfgomezc/PDFViewerExt/blob/master/Capturas/PDFViewerExt1.png" width="300" height="500">
+
+<img src="https://github.com/sfgomezc/PDFViewerExt/blob/master/Capturas/PDFViewerExt2.png" width="300" height="500">
+
+<img src="https://github.com/sfgomezc/PDFViewerExt/blob/master/Capturas/PDFViewerExt3.png" width="300" height="500">
+
